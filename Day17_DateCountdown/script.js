82c461402869cfc3421e8f054da1d78dac7b7dbd
Ian Lim <ianlim28@hotmail.com>
2023-08-14 08:30:06 +0200

    completed JS Day17 challenge

---
+++ Day17_DateCountdown/script.js
@@ -0,0 +1,23 @@
+const countTo = "1 Jan 2024"
+
+const format = (t) => {
+    return t < 10 ? `0${t}` : t;
+}
+
+const c = setInterval(() => {
+
+    const endDate = new Date(countTo);
+    const currentDate = new Date();
+    const totalSeconds = (endDate - currentDate)/1000;
+    const days = Math.floor(totalSeconds / 3600 /24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    const countDown = document.getElementById("countdown");
+    countDown.textContent = `${days} Days ${format(hours)} Hours : ${format(minutes)} Mins :${format(seconds)}s`
+    if(totalSeconds < 0) {
+        clearInterval(c);
+        countDown.textContent = "Happy New Year"
+    }
+
+},1000)
