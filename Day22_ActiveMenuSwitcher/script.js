9a1cf4f45b32cacf54c15803b15d319d7e2dec89
Ian Lim <ianlim28@hotmail.com>
2023-08-20 11:31:04 +0200

    completed JS Day22 challenge

---
+++ Day22_ActiveMenuSwitcher/script.js
@@ -0,0 +1,9 @@
+// sticky header
+window.addEventListener('scroll', () => {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+
+  
