7bbf4e202153aa649d5c1e8c7022c7cda8b9b258
Ian Lim <ianlim28@hotmail.com>
2023-09-11 07:30:43 +0200

    completed JS Day49 challenge

---
+++ Day49_DataAttributeAndJS/script.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll(".btn");
+const text = document.querySelector(".text");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        // first option
+        // const filter = e.target.getAttribute("data-link");
+        // second option
+        const filter = e.target.dataset.link;
+        console.log(filter);
+        if (filter == 'home') {
+            text.textContent = "Home Page";
+        } else if (filter == 'about') {
+            text.textContent = "About Page";
+        } else {
+            text.textContent = "Contact Page";
+        }
+    })
+});
